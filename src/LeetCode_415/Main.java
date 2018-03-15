00777c69a85714c8ba5e6b5b34f945a31e8fc687
William-Hai <return_zero0@163.com>
2018-03-16 01:47:18 +0800

    .

---
+++ src/LeetCode_415/Main.java
@@ -0,0 +1,9 @@
+package LeetCode_415;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        String result = solution.addStrings("", "");
+        System.out.println(result);
+    }
+}
---
+++ src/LeetCode_415/Solution.java
@@ -0,0 +1,7 @@
+package LeetCode_415;
+
+public class Solution {
+    public String addStrings(String num1, String num2) {
+        return "";
+    }
+}
