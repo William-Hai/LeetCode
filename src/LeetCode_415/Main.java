2424f389f7ee1dfb1311615590211e403612932c
William-Hai <return_zero0@163.com>
2018-03-16 02:38:30 +0800

    commit 415 by accepted.

--- src/LeetCode_415/Main.java
+++ src/LeetCode_415/Main.java
@@ -3,7 +3,7 @@ package LeetCode_415;
 public class Main {
     public static void main(String[] args){
         Solution solution = new Solution();
-        String result = solution.addStrings("", "");
+        String result = solution.addStrings("0", "0");
         System.out.println(result);
     }
 }
