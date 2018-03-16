c2714a7ef3c5b0523a340d589fe10e8e096da6f5
William-Hai <return_zero0@163.com>
2018-03-16 15:59:09 +0800

    commit 43 by Accepted.

---
+++ src/LeetCode_043/Main.java
@@ -0,0 +1,9 @@
+package LeetCode_043;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        String result = solution.multiply("123", "456");
+        System.out.println(result);
+    }
+}
