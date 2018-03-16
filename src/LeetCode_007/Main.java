d1817dd2ddc7a8f6d0bda51eed0746dc48fa3d25
William-Hai <return_zero0@163.com>
2018-03-16 19:03:06 +0800

    Commit history(007,009) by Accepted.

---
+++ src/LeetCode_007/Main.java
@@ -0,0 +1,9 @@
+package LeetCode_007;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        int result = solution.reverse(123);
+        System.out.println(result);
+    }
+}
