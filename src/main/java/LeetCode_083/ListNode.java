c1f64c6552a2d26586c9ec6afc16f593c2cd1bcf
Q-WHai <return_zero0@163.com>
2019-03-25 23:06:03 +0800

    add [83. Remove Duplicates from Sorted List]

---
+++ src/main/java/LeetCode_083/ListNode.java
@@ -0,0 +1,9 @@
+package LeetCode_083;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/test/java/LeetCode_083/SolutionTest.java
@@ -0,0 +1,38 @@
+package LeetCode_083;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SolutionTest {
+
+    @Test
+    public void test1() {
+        ListNode ln = new ListNode(1);
+        ln.next = new ListNode(1);
+        ln.next.next = new ListNode(2);
+
+        Assert.assertEquals("1->2", output(new Solution().deleteDuplicates(ln)));
+    }
+
+    @Test
+    public void test2() {
+        ListNode ln = new ListNode(1);
+        ln.next = new ListNode(1);
+        ln.next.next = new ListNode(2);
+        ln.next.next.next = new ListNode(3);
+        ln.next.next.next.next = new ListNode(3);
+
+        Assert.assertEquals("1->2->3", output(new Solution().deleteDuplicates(ln)));
+    }
+
+    private String output(ListNode ln) {
+        String s = "";
+        ListNode tln = ln;
+        while (null != tln) {
+            s = String.format("%s%s%d", s, 0 == s.length() ? "" : "->", tln.val);
+            tln = tln.next;
+        }
+
+        return s;
+    }
+}
